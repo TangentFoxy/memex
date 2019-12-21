b2b75320effe01a4f5950b6e5d3ec28a0925deff
Rose L. Liverman <paul.liverman.iii@gmail.com>
2019-12-21 01:05:53 -0800

    Update settings.js

--- content/settings.js
+++ content/settings.js
@@ -1,6 +1,6 @@
 const SETTINGS = 
 {
-	STATSNUMTAGS: 5,
+	STATSNUMTAGS: 10,
 	STATSNUMTYPE: 10,
 	LOADANIMNUM: 50,
 	WIDEARTICLE: true,
@@ -13,7 +13,6 @@ const SETTINGS =
 	SHOWDATE: true,
 	SHOWAUTH: true,
 	SHOWTYPE: true,
-	SHOWDONE: true,
 	SHOWLINK: true,
 	SHOWLOWER: true,
 	SHOWTAGS: true,
